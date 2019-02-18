38fea4cfe6151a1b749181fef5273fd3e46bd2c5
ketayon <ketayon@gmail.com>
2019-02-18 17:12:11 +0200

    Deploy

--- pizzashopproject/settings.py
+++ pizzashopproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6@@d1hztetfs=i19wad4-dkg+2vmg%^stpe90*vly46df2-@x9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shoppizza.herokuapp.com', 'localhost', '127.0.0.01']
 
